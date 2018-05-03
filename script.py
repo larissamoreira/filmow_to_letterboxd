d4e323c5fd82290a738cb491f48a9cab6bbd5097
larissamoreira <larissamgusmao@gmail.com>
2018-05-03 20:24:36 -0300

    reading all watched movies

--- script.py
+++ script.py
@@ -1,14 +1,17 @@
-#reading movies (for now only in the first page)
+#reading all watched movies
 
 import requests
 from bs4 import BeautifulSoup
 
 user = 'nnothumann' #change this user for yours.
-html_doc = requests.get('https://filmow.com/usuario/%s/filmes/ja-vi/' % user)
-soup = BeautifulSoup(html_doc.text, 'html.parser')
-
 movies = []
-for link in soup.find_all("span", class_="title"):
-    movies.append(link.get_text())
+
+i = 1
+while requests.get('https://filmow.com/usuario/%s/filmes/ja-vi/?pagina=%d' % (user, i)):
+    html_doc = requests.get('https://filmow.com/usuario/%s/filmes/ja-vi/?pagina=%d' % (user, i))
+    i = i + 1
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
+    for link in soup.find_all("span", class_="title"):
+        movies.append(link.get_text())
 
 print(movies)
